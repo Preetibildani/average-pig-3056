435f73de0c7cd469f8e8e22a1c3a93ea99834029
fw20-0769 <tarakkarmakar1434@gmail.com>
2022-12-17 01:03:13 +0530

    p

--- src/Pages/MainRoutes.jsx
+++ src/Pages/MainRoutes.jsx
@@ -3,11 +3,15 @@ import { Route, Routes } from 'react-router'
 // import "./AdminLogin"
 
 import Login from './AdminPage/Login'
+import ConfirmBook from './ConfirmBook/ConfirmBook'
 import UserLogin from './Login/Login'
+import PaymentPage from './Payment/Payement'
 import CreateAccount from './SignUp/SignUp'
 const MainRoutes = () => {
     return (
       <Routes>
+        <Route path='/payment' element={<PaymentPage />} />
+        <Route path='/confirmbooking' element={<ConfirmBook />} />
         <Route path='/login' element={<UserLogin />} />
         <Route path='/SignUp' element={<CreateAccount  />} />
       <Route path='/' element={<Login/>}/>
