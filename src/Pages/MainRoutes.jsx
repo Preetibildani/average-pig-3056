67c93fdf04bf9d8cf594ae4f178ba70ace3c3313
Ayush Chandra <ace9009@outlook.com>
2022-12-16 10:23:30 +0530

    day-3

---
+++ src/Pages/MainRoutes.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Route, Routes } from 'react-router'
+ import Adminpanel from './AdminPage/Adminpanel'
+import Login from './AdminPage/Login'
+import UserLogin from './Login/Login'
+import CreateAccount from './SignUp/SignUp'
+import Hotels from './Hotels/Hotels';
+import Edit from './AdminPage/Edit';
+
+
+const MainRoutes = () => {
+    return (
+      <Routes>
+        <Route path='/login' element={<UserLogin />} />
+        <Route path='/SignUp' element={<CreateAccount  />} />
+        <Route path='/hotels' element={<Hotels/>}/>
+        <Route path='/adminlogin' element={<Login/>}/>
+      <Route path='/admin' element={<Adminpanel/>}/>
+      <Route path='/product/:id' element={<Edit/>}/>
+      </Routes>
+    )
+}
+
+export default MainRoutes;
