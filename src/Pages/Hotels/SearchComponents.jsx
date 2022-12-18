f4cd80cb4f9d0b78069376359fe76d8b8ca1d7cd
Ayush Chandra <ace9009@outlook.com>
2022-12-18 19:06:13 +0530

    search addded

---
+++ src/Pages/Hotels/SearchComponents.jsx
@@ -0,0 +1,20 @@
+import { Box, Flex } from '@chakra-ui/react'
+import React from 'react'
+import { Button } from '@chakra-ui/react';
+import "./Hotels.css"
+const SearchComponents = () => {
+  return (
+    <Box>
+        <Flex>
+            <Box className='topSearch'>a</Box>
+            <Box className='topSearch'>a</Box>
+            <Box className='topSearch'>a</Box>
+            <Box className='topSearch'>a</Box>
+
+            <Button>Search</Button>
+        </Flex>
+    </Box>
+  )
+}
+
+export default SearchComponents
