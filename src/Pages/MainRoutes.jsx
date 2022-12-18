bf521b80b90a2ef8895d551424b3445aeb8a7758
Raghavendra <raghavendrardeshabhandari@gmail.com>
2022-12-19 03:30:04 +0530

    hotel cart files added

--- src/Pages/MainRoutes.jsx
+++ src/Pages/MainRoutes.jsx
@@ -7,13 +7,15 @@ import UserLogin from './Login/Login'
 import PaymentPage from './Payment/Payement'
 import CreateAccount from './SignUp/SignUp'
 import Hotels from './Hotels/Hotels';
+// import {HotelsCart} from './HotelsCart/HotelsCart';
 import {HotelsCart} from './HotelsCart/HotelsCart'
 import Edit from './AdminPage/Edit'
 import Homepage from './Homepage'
+
 const MainRoutes = () => {
     return (
       <Routes>
-        <Route path='/hotelsCart' element={<HotelsCart/>} 
+        <Route path='/hotelsCart' element={<HotelsCart/>} />
         <Route path='/' element={<Homepage />} />
         <Route path='/login' element={<UserLogin />} />
         <Route path='/SignUp' element={<CreateAccount  />} />
